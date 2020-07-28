08ff11a92664df048351c5e3126aabaf6722faa5
Scott Moss <willscottmoss@gmail.com>
2020-07-28 12:48:41 -0700

    pages

---
+++ pages/notes/[id].jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+export default () => {
+  const router = useRouter()
+  const { id }= router.query
+
+  return (
+    <h1>Note: {id} </h1>
+  )
+}
