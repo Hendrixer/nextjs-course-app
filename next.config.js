ac1a06ef1617f73c0a53dfa603dd312a1f83bcfc
Scott Moss <willscottmoss@gmail.com>
2020-07-29 06:38:22 -0700

    chore

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const nextEnv = require('next-env')
-const dotenvLoad = require('dotenv-load')
-
-dotenvLoad()
-
-const withNextEnv = nextEnv()
-module.exports = withNextEnv()
--- src/data/data.js
+++
@@ -1,8 +0,0 @@
-const notes = new Array(15)
-  .fill(1)
-  .map((_, i) => ({
-    id: Date.now() + i,
-    title: `Note ${i}`
-  }))
-
-module.exports = notes
