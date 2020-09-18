139ed9339bc0078bfffdd36feac0c4587987e981
rlchu <ryan.chupick@gmail.com>
2020-09-17 22:43:25 -0700

    theme ui fun

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+// pages/index.jsx
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import Link from "next/link";
 
-const Page = () => (
-  <div>
-    <h1>Index Page</h1>
-    <Link href="/notes">
-      <a>Note</a>
-    </Link>
+export default () => (
+  <div sx={{ height: `calc(100vh - 60px)` }}>
+    <div
+      sx={{
+        variant: "containers.page",
+        display: "flex",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <h1 sx={{ fontSize: 8, my: 0 }}>
+        This is a really dope note taking app.
+      </h1>
+    </div>
   </div>
 );
-
-export default Page;
--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,4 +1,6 @@
-import React from "react";
+// pages/[id].jsx
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
@@ -7,12 +9,8 @@ export default () => {
   const { id } = router.query;
 
   return (
-    <div>
+    <div sx={{ variant: "containers.page" }}>
       <h1>Note: {id} </h1>
-
-      <Link href="/notes">
-        <a>Notes</a>
-      </Link>
     </div>
   );
 };
